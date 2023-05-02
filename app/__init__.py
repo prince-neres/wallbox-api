39b3090fecee554907f04893e90b28901d18c2a4
Prince Neres <prince84224960@gmail.com>
2023-05-02 00:17:19 -0300

    feat: Adicionando serviço de S3 na aplicação

--- app/__init__.py
+++ app/__init__.py
@@ -4,15 +4,20 @@ from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from config import Config
+from boto3 import client
+from os import getenv
 
 
 db = SQLAlchemy()
+s3 = client('s3', aws_access_key_id=getenv('AWS_ACCESS_KEY_ID'),
+            aws_secret_access_key=getenv('AWS_SECRET_ACCESS_KEY'))
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     jwt = JWTManager(app)
+
     CORS(app)
 
     db.init_app(app)
