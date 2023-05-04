97667c0d526ed8c5090e67d691228a2e81f7a702
Prince Neres <prince84224960@gmail.com>
2023-05-04 02:11:13 -0300

    fix: Alterando tipo de data das colunas de data

--- app/models.py
+++ app/models.py
@@ -8,8 +8,8 @@ class User(db.Model):
     username = db.Column(db.String(100), nullable=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(200), nullable=True)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow())
-    date_updated = db.Column(db.DateTime, default=datetime.utcnow())
+    date_created = db.Column(db.DateTime, default=datetime.now())
+    date_updated = db.Column(db.DateTime, default=datetime.now())
 
 
 class Wallpaper(db.Model):
@@ -20,5 +20,5 @@ class Wallpaper(db.Model):
     tags = db.Column(ARRAY(db.String(50)), nullable=False, default=[])
     description = db.Column(db.String(500), nullable=False)
     image = db.Column(db.String(500), nullable=False, default='')
-    date_created = db.Column(db.DateTime, default=datetime.utcnow())
-    date_updated = db.Column(db.DateTime, default=datetime.utcnow())
+    date_created = db.Column(db.DateTime, default=datetime.now())
+    date_updated = db.Column(db.DateTime, default=datetime.now())
