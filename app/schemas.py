19f2b38a0ff663d2e618cf38ece97defb5610b44
Prince Neres <prince84224960@gmail.com>
2023-05-04 19:29:47 -0300

    feat: Usando schemas para serializar entidades

---
+++ app/schemas.py
@@ -0,0 +1,19 @@
+from marshmallow import Schema, fields
+
+
+class UserSchema(Schema):
+    id = fields.Int()
+    username = fields.Str()
+    email = fields.Email()
+    date_created = fields.DateTime(format='%Y-%m-%d %H:%M:%S')
+    date_updated = fields.DateTime(format='%Y-%m-%d %H:%M:%S')
+
+
+class WallpaperSchema(Schema):
+    id = fields.Int(dump_only=True)
+    title = fields.Str(required=True)
+    description = fields.Str(required=True)
+    tags = fields.List(fields.Str(), required=True)
+    image = fields.Str(required=True)
+    date_created = fields.DateTime(dump_only=True)
+    date_updated = fields.DateTime(dump_only=True)
