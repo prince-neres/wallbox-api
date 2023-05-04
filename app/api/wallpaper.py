43593906d83f045cfcf6861a2707129b632485e6
Prince Neres <prince84224960@gmail.com>
2023-05-04 19:30:21 -0300

    feat: Criando endpoints para deletar, editar e ler wallpaper

--- app/api/wallpaper.py
+++ app/api/wallpaper.py
@@ -2,10 +2,60 @@ from flask import make_response, jsonify, request
 from flask_cors import cross_origin
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from os import getenv
+from datetime import datetime
 from . import api
 from app import db, Config, s3
 from app.models import Wallpaper, User
+from app.schemas import WallpaperSchema, UserSchema
 import uuid
+
+
+@api.route('/wallpaper/<int:id>', methods=['GET'])
+@jwt_required()
+@cross_origin(origins=Config.CLIENT_URL)
+def get_wallpaper(id):
+    wallpaper = Wallpaper.query.filter_by(id=id).first_or_404(
+        description=f"Wallpaper with id {id} not found")
+    wallpaper_schema = WallpaperSchema()
+    result = wallpaper_schema.dump(wallpaper)
+    return make_response(jsonify(result), 200)
+
+
+@api.route('/wallpapers', methods=['GET'])
+@cross_origin(origins=Config.CLIENT_URL)
+def get_wallpapers():
+    # Utiliza join para obter os dados do usuário associado a cada imagem
+    wallpapers = db.session.query(Wallpaper, User).join(
+        User, Wallpaper.user_id == User.id).all()
+
+    wallpapers_list = []
+
+    for wallpaper, user in wallpapers:
+        user_schema = UserSchema()
+        user_json = user_schema.dump(user)
+
+        wallpaper_schema = WallpaperSchema()
+        wallpaper_json = wallpaper_schema.dump(wallpaper)
+
+        # Inclui o JSON do usuário dentro do JSON do wallpaper
+        wallpaper_json['user'] = user_json
+
+        wallpapers_list.append(wallpaper_json)
+
+    return make_response(jsonify(wallpapers_list), 200)
+
+
+@api.route('/user-wallpapers', methods=['GET'])
+@cross_origin(origins=Config.CLIENT_URL)
+@jwt_required()
+def get_user_wallpapers():
+    user_id = get_jwt_identity().get('id')
+    user_wallpapers = Wallpaper.query.filter_by(user_id=user_id).all()
+
+    wallpaper_schema = WallpaperSchema(many=True)
+    result = wallpaper_schema.dump(user_wallpapers)
+
+    return make_response(jsonify(result), 200)
 
 
 @api.route('/upload_wallpaper', methods=['POST'])
@@ -58,17 +108,12 @@ def upload_image():
             user_id=user.get('id'), title=title, description=description, filename=filename, image=url, tags=tags)
         db.session.add(new_wallpaper)
         db.session.commit()
-        data = {
-            'id': new_wallpaper.id,
-            'title': new_wallpaper.title,
-            'description': new_wallpaper.description,
-            'tags': new_wallpaper.tags,
-            'filename': new_wallpaper.filename,
-            'image': new_wallpaper.image,
-            'date_created': new_wallpaper.date_created,
-            'date_updated': new_wallpaper.date_updated,
-        }
-        return make_response(jsonify(data), 201)
+
+        # Cria os objetos de schema e faz o dump dos dados
+        wallpaper_schema = WallpaperSchema()
+        wallpaper_data = wallpaper_schema.dump(new_wallpaper)
+
+        return make_response(jsonify(wallpaper_data), 201)
     except:
         # Erro genérico
         error_data = {
@@ -78,63 +123,56 @@ def upload_image():
         return make_response(jsonify(error_data), 500)
 
 
-@api.route('/wallpapers', methods=['GET'])
-@cross_origin(origins=Config.CLIENT_URL)
-def get_wallpapers():
-    # Utiliza join para obter os dados do usuário associado a cada imagem
-    wallpapers = db.session.query(Wallpaper, User).join(
-        User, Wallpaper.user_id == User.id).all()
-
-    wallpapers_list = []
-
-    for wallpaper, user in wallpapers:
-        user_json = {
-            'id': user.id,
-            'username': user.username,
-            'email': user.email,
-            'date_created': user.date_created,
-            'date_updated': user.date_updated
-        }
-
-        wallpaper_json = {
-            'id': wallpaper.id,
-            'user': user_json,
-            'title': wallpaper.title,
-            'description': wallpaper.description,
-            'tags': wallpaper.tags,
-            'filename': wallpaper.filename,
-            'image':  wallpaper.image,
-            'date_created': wallpaper.date_created,
-            'date_updated': wallpaper.date_updated,
-        }
-
-        wallpapers_list.append(wallpaper_json)
-
-    return make_response(jsonify(wallpapers_list), 200)
-
-
-@api.route('/user-wallpapers', methods=['GET'])
-@cross_origin(origins=Config.CLIENT_URL)
+@api.route('/wallpaper/<int:id>', methods=['DELETE'])
 @jwt_required()
-def get_user_wallpapers():
+@cross_origin(origins=Config.CLIENT_URL)
+def delete_wallpaper(id):
+    wallpaper = Wallpaper.query.filter_by(id=id).first_or_404(
+        description=f"Wallpaper with id {id} not found")
+    db.session.delete(wallpaper)
+    db.session.commit()
+    return make_response(jsonify({'message': 	'Wallpaper deletado com sucesso!', 'code': 'SUCCESS'}), 200)
+
+
+@api.route('/wallpaper/<int:id>', methods=['PUT'])
+@jwt_required()
+@cross_origin(origins=Config.CLIENT_URL)
+def update_wallpaper(id):
+    # Obtém dados da requisição
     user = get_jwt_identity()
-    user_wallpapers = Wallpaper.query.filter_by(user_id=user.get("id"))
+    form_data = request.form
+    title = form_data.get('title')
+    tags = eval(form_data.get('tags'))
+    description = form_data.get('description')
 
-    user_wallpapers_list = []
-
-    for wallpaper in user_wallpapers:
-
-        wallpaper_json = {
-            'id': wallpaper.id,
-            'title': wallpaper.title,
-            'description': wallpaper.description,
-            'tags': wallpaper.tags,
-            'filename': wallpaper.filename,
-            'image':  wallpaper.image,
-            'date_created': wallpaper.date_created,
-            'date_updated': wallpaper.date_updated,
+    if not title or not description or not tags:
+        error_data = {
+            'message': 'O campo título, descrição e tags precisam ser preenchidos',
+            'code': 'INVALID_DATA'
         }
+        return make_response(jsonify(error_data), 400)
 
-        user_wallpapers_list.append(wallpaper_json)
+    # Verifica se o wallpaper existe
+    wallpaper = Wallpaper.query.filter_by(id=id).first_or_404(
+        description=f"Wallpaper with id {id} not found")
 
-    return make_response(jsonify(user_wallpapers_list), 200)
+    # Verifica se o usuário é dono do wallpaper
+    if wallpaper.user_id != user.get('id'):
+        error_data = {
+            'message': 'Usuário não autorizado a atualizar este wallpaper',
+            'code': 'UNAUTHORIZED'
+        }
+        return make_response(jsonify(error_data), 401)
+
+    # Atualiza informações do wallpaper
+    wallpaper.title = title
+    wallpaper.description = description
+    wallpaper.tags = tags
+    wallpaper.date_updated = datetime.now()
+    db.session.commit()
+
+    # Cria o objeto de retorno
+    wallpaper_schema = WallpaperSchema()
+    data = wallpaper_schema.dump(wallpaper)
+
+    return make_response(jsonify(data), 200)
